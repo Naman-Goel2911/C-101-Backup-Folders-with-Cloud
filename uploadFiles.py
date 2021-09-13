208017009a55863ce8474cf4eff25e36dc2a1eb8
Naman-Goel2911 <73833786+Naman-Goel2911@users.noreply.github.com>
2021-09-13 18:21:15 +0530

    Add files via upload

--- uploadFiles.py
+++ uploadFiles.py
@@ -11,19 +11,20 @@ class TransferData:
 
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
-        local_path = 'C:/Users/goeln/Desktop/School'
 
         for rootFolder, folders, files in os.walk(file_from):
-            relative_path = os.path.relpath(local_path, file_from)
-            dropbox_path = os.path.join(file_to, relative_path)
-            with open(local_path, 'rb') as f:
-                dbx.files_upload(f.read(), dropbox_path, mode = WriteMode('overwrite')) 
+            for fileName in files:
+                local_path = os.path.join(rootFolder, fileName)
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, fileName)
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode = WriteMode('overwrite')) 
 
 def main():
-    access_token = 'sl.A4CFXvFCJrfQbIXklUa1O_xgZrcc9VocelHiOHS3ybnKEOaCM_ChkeCx27pt4qX7m4phZTQr65KpWo5JnKOX6GmCW5TtHzxUDQK4_tLSB_Moy2weeDr-lJ5v9NHMmgOkVUCt_lQ'
+    access_token = 'sl.A4NXMsNNZd0Zr1pzsVR3sleOMaNNAxARK8vu5dV_nUH4X6ZLTrG3At2cvgGdv_fLLtNziHxnVEf9PJlhpwc9baWo_OKO7hLbEw1w0VO1ma9BKXQ1WfjTdMjVLvXy1rRbRBY4NXs'
     transferData = TransferData(access_token)
 
-    file_from = input('Enter the file/folder you to be backed up: ')
+    file_from = str(input('Enter the file/folder you to be backed up: '))
     file_to = input('Enter the full path to upload to Dropbox: ')
 
 
